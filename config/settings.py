ae6e663638f4e48ac05da1e7a1db7b4264ebcd94
Lennox Charles <lennyaiko17@gmail.com>
2023-07-10 22:35:26 +0100

    fix: cors whitelist

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,8 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3001',
     'http://localhost:3005',
     'http://localhost:5173',
-    'http://localhost:4000'
+    'http://localhost:4000',
+    'https://www.hairsenseretail.com/'
 ]
 CORS_ALLOW_HEADERS = [
     "accept",
